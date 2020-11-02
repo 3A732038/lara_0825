1fb6e9501f6ea67711ec602ff27cf6c1bc57ef1e
Wu Yuzhe <a0903153387@yahoo.com.tw>
2020-11-02 23:08:31 +0800

    設定路由和建立PostsController

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,13 @@ use Illuminate\Support\Facades\Route;
 /*Route::get('/', function () {
     return view('contact');
 });*/
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('about');
+});*/
+Route::get('/', function () {
+    return view('welcome');
 });
+Route::get('posts', [\App\Http\Controllers\PostsController::class,'index'])->name('posts.index');
+Route::get('post', [\App\Http\Controllers\PostsController::class,'show'])->name('posts.show');
+Route::get('about', [\App\Http\Controllers\PostsController::class,'about'])->name('posts.about');
+Route::get('contact', [\App\Http\Controllers\PostsController::class,'contact'])->name('posts.contact');
