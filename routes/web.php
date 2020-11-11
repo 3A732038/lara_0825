b4356134ec7193f5b104b692f61fbebd1e7ecc04
Wu Yuzhe <a0903153387@yahoo.com.tw>
2020-11-11 10:30:02 +0800

    練習2.5:用 new 的方式新增資料

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,7 +26,11 @@ use Illuminate\Support\Facades\Route;
     return view('about');
 });*/
 Route::get('/', function () {
-    return view('welcome');
+    $post=new Post();
+    $post->title='test title';
+    $post->content='test content';
+    $post->save();
+
 });
 Route::get('posts', [\App\Http\Controllers\PostsController::class,'index'])->name('posts.index');
 Route::get('post', [\App\Http\Controllers\PostsController::class,'show'])->name('posts.show');
